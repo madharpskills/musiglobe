e8b79205416b9d6c04106bb4e7e09d5cccfecacf
Will Alleman <will.alleman@chghealthcare.com>
2020-10-01 11:41:31 -0600

    feat: add apicache middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 const express = require('express')
 const path = require('path')
+const apicache = require('apicache')
 const { getSongData, getArtistData, getSpotifyData } = require('./utils/displayMusicData')
 const dotenv = require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000
+const cache = apicache.middleware
 
 app.use(express.static(path.join(__dirname, '../public')))
+app.use(cache('1 minutes'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/global', async (req, res) => {
+    console.log('getting global data')
     const data = await getSpotifyData('global')
     res.send({ globalData: data[1] })
 })
 
 app.get('/country', async (req, res) => {
+    console.log(`getting data for ${req.query.code}`)
     let song = await getSongData(req.query.code)
     let artist = getArtistData(req.query.code)
     res.send({ topSong: song, topArtist: artist})
