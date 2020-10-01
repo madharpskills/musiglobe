16ceb79f78a0bcb7a4d626ede8f0d92259a1b0f3
Deborah Richins <deborah.richins@chghealthcare.com>
2020-10-01 11:31:29 -0600

    Revert "Revert "feat: look up spotify song for non-spotify countries""
    This reverts commit 0c89a4cbeaa8a3bf5034fd71e88163ec55754703.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const { getSongData, getArtistData, getSpotifyData } = require('./utils/displayMusicData')
+const dotenv = require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 3000
---
+++ src/utils/spotify.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+const qs = require('qs')
+
+let token
+let currentDateTime
+let expirationDateTime
+
+const getSpotifyToken = async () => {
+    currentDateTime = new Date()
+    if (!token || (currentDateTime - expirationDateTime) > 3590000) {
+        try {
+            const res = await axios.post('https://accounts.spotify.com/api/token', qs.stringify({ 
+                'grant_type': 'client_credentials' 
+            }), {
+                headers: { 'Authorization': process.env.AUTH, 'Content-Type': 'application/x-www-form-urlencoded' },
+            })
+            expirationDateTime = new Date()
+            token = res.data.access_token
+            return token
+        } catch (e) {
+            console.log(e)
+        }
+    } else {
+        return token
+    }
+}
+
+const getSpotifySearchQuery = async (track, artist) => {
+    try {
+        const res = await axios.get(`https://api.spotify.com/v1/search?q=track:${track}%20artist:${artist}&type=track,artist`, {
+            headers: { 'Authorization': `Bearer ${await getSpotifyToken()}` }
+        })
+        return res.data.tracks.items[0].external_urls.spotify
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+module.exports = {
+    getSpotifySearchQuery
+}
