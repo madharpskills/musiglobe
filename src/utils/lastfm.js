285c2fc8e359e5b6d0108ec4e4e559dfc7df84aa
Deborah Richins <deborah.richins@chghealthcare.com>
2020-10-02 10:15:08 -0600

    secretify lastfm token

--- src/utils/lastfm.js
+++ src/utils/lastfm.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
+const lastFmKey = process.env.LASTFM_KEY
 
 const getTopTrack = async (country) => {
     try {
-        const res = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=fa1561766a2c55ddd4ea3088f5c89416&format=json&limit=1`)
+        const res = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=${lastFmKey}&format=json&limit=1`)
         return res.data.tracks.track
     } catch (e) {
         console.error(e, country)
@@ -11,7 +12,7 @@ const getTopTrack = async (country) => {
 
 const getTopArtist = async (country) => {
     try {
-        const res = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=fa1561766a2c55ddd4ea3088f5c89416&format=json&limit=1`)
+        const res = await axios.get(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${lastFmKey}&format=json&limit=1`)
         return res.data.topartists.artist
     } catch (e) {
         console.error(e, country)
