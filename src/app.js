2772e8a14acdbc1be55c826eda738181f73f6cb0
Deborah Richins <deborah.richins@chghealthcare.com>
2020-10-02 10:02:48 -0600

    merge

--- src/app.js
+++ src/app.js
@@ -18,5 +18,5 @@ app.get('/country', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`)
+    console.log(`Listening to requests on port ${port}`)
 }) 
